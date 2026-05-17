caa994177285926c5ace665138fe596a26b3a76b
Arjan <arjanpingale@gmail.com>
2026-05-17 09:28:12 +0530

    Removed stale TODO's

--- frontend/src/features/gmail/components/message-display.tsx
+++ frontend/src/features/gmail/components/message-display.tsx
@@ -138,7 +138,6 @@ export function MessageDisplay() {
   return (
     <div className="flex h-full flex-col">
       {/* Toolbar */}
-      {/* TODO: add functionality to toolbar buttons */}
       <div className="flex items-center p-2">
         <div className="flex items-center gap-2">
           <Button
@@ -312,7 +311,6 @@ export function MessageDisplay() {
         <Separator />
 
         {/* Reply */}
-        {/* TODO: add send mail functionality after implementing /api/gmail/send endpoint in backend */}
         <div className="p-4">
           <div className="grid gap-4">
             <Textarea
